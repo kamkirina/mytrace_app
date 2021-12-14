ffd09b5c891ffb604180870664d1ebcacde91a85
Irina <70759778+kamkirina@users.noreply.github.com>
2021-12-14 17:41:19 +0300

    delete

--- force-app/main/default/aura/CTLocationView/CTLocationViewController.js
+++
@@ -1,8 +0,0 @@
-({
-    locationSelect: function (component, event, helper) {
-        const recordId = event.getParam("recordId");
-        const status = event.getParam("status");
-        component.set("v.recordId", recordId);
-        component.set("v.status", status);
-    }
-});
